b641339b742617799913558542b9e0b37e4889af
cenyu <kcenyu10@126.com>
2022-08-13 15:10:38 +0800

    添加功能：数据展示，数据搜索，分页，删除医院设置

--- src/api/hospset.js
+++ src/api/hospset.js
@@ -1,11 +1,19 @@
 import request from '@/utils/request'
 
 export default {
+  // 医院设置列表
   getHospSetList(current, limit, searchObj) {
     return request({
       url: `/admin/hosp/hospitalSet/findPageHospSet/${current}/${limit}`,
       method: 'post',
       data: searchObj // 因为后端使用@RequestBody接受查询条件参数，因此前端需要将查询条件转为JSON发送给后端接口
+    })
+  },
+  // 删除医院设置
+  deleteHospSet(id) {
+    return request({
+      url: `/admin/hosp/hospitalSet/delete/${id}`,
+      method: 'delete'
     })
   }
 }
--- src/views/hospset/list.vue
+++ src/views/hospset/list.vue
@@ -1,6 +1,50 @@
 <template>
   <div class="app-container">
-    医院设置列表
+    医院设置列表 <br><br>
+    <el-form :inline="true" class="demo-form-inline">
+      <el-form-item>
+        <el-input v-model="searchObj.hosname" placeholder="医院名称"/>
+      </el-form-item>
+      <el-form-item>
+        <el-input v-model="searchObj.hoscode" placeholder="医院编号"/>
+      </el-form-item>
+      <el-button type="primary" icon="el-icon-search" @click="getList()">查询</el-button>
+    </el-form>
+    <!-- 表格数据 -->
+    <el-table
+      :data="list"
+      stripe
+      style="width: 100%">
+      <el-table-column type="index" width="50" label="序号"/>
+      <el-table-column prop="hosname" label="医院名称"/>
+      <el-table-column prop="hoscode" label="医院编号"/>
+      <el-table-column prop="apiUrl" label="api基础路径" width="200"/>
+      <el-table-column prop="contactsName" label="联系人姓名"/>
+      <el-table-column prop="contactsPhone" label="联系人手机"/>
+      <el-table-column label="状态" width="80">
+        <template slot-scope="scope">
+          {{ scope.row.status === 1 ? '可用' : '不可用' }}
+        </template>
+      </el-table-column>
+      <el-table-column label="操作" width="280" align="center">
+        <template slot-scope="scope">
+          <el-button
+            type="danger"
+            size="mini"
+            icon="el-icon-delete"
+            @click="removeDataById(scope.row.id)"/>
+        </template>
+      </el-table-column>
+    </el-table>
+    <!-- 分页 -->
+    <el-pagination
+      :current-page="current"
+      :page-size="limit"
+      :total="total"
+      style="padding: 30px 0; text-align: center;"
+      layout="total, prev, pager, next, jumper"
+      @current-change="getList"
+    />
   </div>
 </template>
 
@@ -21,7 +65,8 @@ export default {
     this.getList()
   },
   methods: { // 定义方法，请求调用接口
-    getList() {
+    getList(page = 1) { // 添加当前页参数
+      this.current = page
       hospset.getHospSetList(this.current, this.limit, this.searchObj)
         .then(response => {
           this.list = response.data.records
@@ -30,6 +75,29 @@ export default {
         .catch(error => {
           console.log(error)
         })
+    },
+    removeDataById(id) { // 删除医院设置
+      this.$confirm('此操作将永久删除医院设置信息, 是否继续?', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(() => {
+        hospset.deleteHospSet(id)
+          .then(response => {
+            // 提示信息
+            this.$message({
+              type: 'success',
+              message: '删除成功!'
+            })
+            // 刷新页面
+            this.getList()
+          })
+      }).catch(() => {
+        this.$message({
+          type: 'info',
+          message: '已取消删除'
+        })
+      })
     }
   }
 }
